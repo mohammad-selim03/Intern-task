15e11033d6e993a2624a701631e79cc261f67cb3
selimnill <mrrafi812345@gmail.com>
2024-09-19 16:15:12 +0600

    config

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import SearchIcon from "./SearchIcon.png";
+import menImg from "./menImg.png";
+import Rectangle from "./Rectangle.png";
+import wared from "./wared.png";
+
+
+
+
+export {SearchIcon, menImg, Rectangle, wared};
